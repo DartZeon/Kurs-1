2a5565a28455963ae575b5afbe6e9140d5d85cf4
Dart Zeon <dart.leonid@gmail.com>
2019-04-14 10:58:07 +0300

    lab4_add

---
+++ lab4/lab4/lab4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab4
+//
+//  Created by D1 on 4/14/19.
+//  Copyright © 2019 D1. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
